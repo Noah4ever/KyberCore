911519a62ddade5aa7871fb85e31a09b13934f25
Noah Thiering <noah.thiering@gmail.com>
2025-04-30 19:48:00 +0200

    [refactor] main.cpp includes

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,27 @@
 #include "Arduino.h"
-#include <Wire.h>
-#include <map>
-#include <ArduinoJson.h>
 
 #include "core/LightsaberController.hpp"
+#include "components/sdcard/ESP32SDCardIO.hpp"
+#include "components/audio/ESP32SDCardAudioPlayer.hpp"
 
-#include "core/DisplayManager.hpp"
+/*
+  TODO: Implement a UI Builder to create the UI in the States to emit the created object to the display
+  TODO: Implement all settings get a setup call to publish the initial state
+  TODO: Implement Blaster Reflect, Blade Lock, and other blade effects
+  TODO: Implement a state machine for the blade to handle the blade effects ?? Maybe. was created by github copilot
+  TODO: Implement more colors. not only rgb but also animation of the colors and variations of the colors like unstable
+  TODO: Create menu state with submenu, scroll lists, etc. padding, container direction: vertical/horizontal.
+    When in the menu use the rotary encoder to scroll through the menu items and select them with the button.
+*/
 
-#include "core/ComponentFactory.hpp"
-#include "components/ComponentType.hpp"
-
-#include "core/SettingsStateMachine.hpp"
-#include "states/BrightnessState.hpp"
-#include "states/ColorState.hpp"
-#include "states/VolumeState.hpp"
-#include <components/sdcard/ESP32SDCardIO.hpp>
-#include <components/audio/ESP32SDCardAudioPlayer.hpp>
-
-// Main controller for lightsaber functionality (blade ignition, sound, etc.)
+// Main controller for lightsaber functionality
 LightsaberController *lightsaberController;
-
-// JSON config: defines hardware setup for blade, IMU, encoder, etc.
-
 
 void setup()
 {
   Serial.begin(115200);
 
+  // Initialize SDCard IO
   ESP32SDCardIO* sdCardIO = new ESP32SDCardIO(27);
   if (!sdCardIO->initializeSD()) {
       while (1) {
@@ -34,26 +29,18 @@ void setup()
           delay(1000);
       }
   }
-  
+  // Initialize Audio Player
   ESP32SDCardAudioPlayer* audioPlayer = new ESP32SDCardAudioPlayer(sdCardIO);
 
+  // Initialize Lightsaber Controller
   lightsaberController = new LightsaberController(sdCardIO, audioPlayer);
   lightsaberController->setup();
-
 }
 
-/*
-2.  TODO: Implement a UI Builder to create the UI in the States to emit the created object to the display
-3.  TODO: Implement all settings get a setup call to publish the initial state
-4.  TODO: Implement Blaster Reflect, Blade Lock, and other blade effects
-5.  TODO: Implement a state machine for the blade to handle the blade effects ?? Maybe. was created by github copilot
-6.  TODO: Implement more colors. not only rgb but also animation of the colors and variations of the colors like unstable
-4.  TODO: Create menu state with submenu, scroll lists, etc. padding, container direction: vertical/horizontal.
-  When in the menu use the rotary encoder to scroll through the menu items and select them with the button.
-*/
 void loop()
 {
+  // Update the lightsaber controller
   lightsaberController->update();
-
-  delay(1); // Short pause to yield CPU to other tasks/peripherals
+  // Short delay
+  delay(1);
 }
